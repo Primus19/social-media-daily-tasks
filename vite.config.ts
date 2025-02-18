151813279d8dcc06ed83ad563d3e1c110d2ad754
Primus19 <primus.vekuh@gmail.com>
2025-02-18 00:01:35 -0600

    check in

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-build: {
-  outDir: 'dist', // Explicitly setting output directory
-  emptyOutDir: true,
-},
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    hmr: false  // Disable Hot Module Replacement (HMR) for production
   },
 });
+
